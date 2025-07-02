7d06076a3619159bc5dc01470545dca3a820d618
guziyi <a1725137488@gmail.com>
2025-07-02 10:54:45 +0800

    feat:修改备案号

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -3,15 +3,15 @@
     <template #page-bottom>
       <p class="copyright">
         <!-- <img v-if="badge" :src="badge" alt />  -->
-        | Copyright © 京ICP备18000331号-1
-      </p> 
+        | Copyright © 京ICP备2025132706号
+      </p>
     </template>
   </Layout>
 </template>
 
 <script>
 import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
-import { ref,watch } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 export default {
   components: {
@@ -21,8 +21,8 @@ export default {
     const route = useRoute()
     let badge = ref(`https://visitor-badge.glitch.me/badge?page_id=shengxinjing-cn.${route.path}`)
     watch(
-      ()=>route.path,
-      path=>{
+      () => route.path,
+      path => {
         badge.value = `https://visitor-badge.glitch.me/badge?page_id=shengxinjing-cn.${path}`
       },
     )
@@ -35,23 +35,26 @@ export default {
 .my-footer {
   text-align: right;
   position: relative;
-  padding-right:3px;
+  padding-right: 3px;
   height: 0;
   max-width: var(--content-width);
   margin: 0 auto;
 
 }
+
 .my-footer img {
   position: relative;
   top: -38px;
 }
-.copyright{
+
+.copyright {
   text-align: center;
   line-height: 1;
-  font-size:14px;
-  margin:0;
+  font-size: 14px;
+  margin: 0;
 }
-.copyright img{
+
+.copyright img {
   vertical-align: middle;
 }
 </style>
